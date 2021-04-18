6b658172dc1302503172d9df279dd60883ee75d2
AlejandroFlores <yaro.flores8816@est.uca.edu.ni>
2021-04-18 01:22:43 -0600

    Entidades de vistas de Distribucion, arbol y paisregion, inserts de arbol y vista arbol

---
+++ src/entidades/VW_PaisRegion.java
@@ -0,0 +1,42 @@
+package entidades;
+
+public class VW_PaisRegion {
+
+	//Atributos
+	private int ID;
+	private String Pais;
+	private String Estado;
+	private String Region;
+	
+	//Metodos
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getPais() {
+		return Pais;
+	}
+	public void setPais(String pais) {
+		Pais = pais;
+	}
+	public String getEstado() {
+		return Estado;
+	}
+	public void setEstado(String estado) {
+		Estado = estado;
+	}
+	public String getRegion() {
+		return Region;
+	}
+	public void setRegion(String region) {
+		Region = region;
+	}
+	
+	
+	
+	
+	
+}
